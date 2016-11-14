07850a7d19a6d7f53871b8250d24cecf353f1776
Kareem <kareem.sorathia@gmail.com>
2016-11-13 18:04:13 -0700

    Fast push

--- hists/muHist.py
+++ hists/muHist.py
@@ -13,15 +13,24 @@ def getdK(h5pFile):
 	dK = Kf/K0
 	return dK
 def getdMu(h5pFile):
+	TINY = 1.0e-8
 	pIds,Muf = lfmpp.getH5pFin(h5pFile ,"Mu")
 	pIds,Mu0 = lfmpp.getH5pInit(h5pFile,"Mu")
+	Ind = (Muf > TINY)
+	print("Cutting %d particles for undefined Mu"%(Ind.sum()))
+	Muf = Muf[Ind]
+	Mu0 = Mu0[Ind]
+
 	dMu = (Muf-Mu0)/Mu0
 	return dMu
 
 #Particle data
-SpcsStub = ["pInj"]
-SpcsLab = ["H+"]
+Stub = "Inj"
+SpcsStubs = ["p","hepp","o"]
+SpcsLab = ["H+","He++","O+"]
 KStubs = [10,25,50]
+
+
 
 dMub = np.linspace(-1,1)
 dKb = np.linspace(0,6,30)
@@ -32,14 +41,14 @@ RootDir = os.path.expanduser('~') + "/Work/Injection/Data"
 vtiDir = RootDir + "/" + "eqSlc"
 h5pDir = RootDir + "/" "H5p"
 
-Ns = len(SpcsStub)
+Ns = len(SpcsStubs)
 Nk = len(KStubs)
 lfmv.initLatex()
 
 for s in range(Ns):
 	for k in range(Nk):
-		h5p = SpcsStub[s] + "%02d"%KStubs[k] + ".All.h5part"
-		figName = SpcsStub[s] + "%02d"%KStubs[k] + ".muHist.png"
+		h5p = SpcsStubs[s] + Stub + "%02d"%KStubs[k] + ".All.h5part"
+		figName = SpcsStubs[s] + "%02d"%KStubs[k] + ".muHist.png"
 		h5pFile = h5pDir + "/" + h5p
 		
 		#Get changes
@@ -56,9 +65,5 @@ for s in range(Ns):
 		plt.ylabel("Energization Fraction, $K_{F}/K_{0}$")
 		plt.savefig(figName)
 		plt.close('all')
-	#plt.hist(Ks,bins,normed=True)
-	#plt.legend(Leg)
-	#plt.xlabel("Final Energy [keV]")
-	#plt.ylabel("Population Density")
 	
 	
