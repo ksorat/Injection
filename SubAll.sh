bbfe9344445e62ed37b4bf0aa036aef0659dce0c
ksorat <kareem.sorathia@gmail.com>
2016-11-30 21:31:36 -0500

    Ready for sweep

---
+++ SubAll.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+lfmtpBatch.sh Inps/p_sInJ.K10.xml 1 1
+lfmtpBatch.sh Inps/p_sInJ.K50.xml 1 1
+lfmtpBatch.sh Inps/p_sInJ.K100.xml 1 1
+lfmtpBatch.sh Inps/O_sInJ.K10.xml 1 1
+lfmtpBatch.sh Inps/O_sInJ.K50.xml 1 1
+lfmtpBatch.sh Inps/O_sInJ.K100.xml 1 1
+lfmtpBatch.sh Inps/He_sInJ.K10.xml 1 1
+lfmtpBatch.sh Inps/He_sInJ.K50.xml 1 1
+lfmtpBatch.sh Inps/He_sInJ.K100.xml 1 1
+lfmtpBatch.sh Inps/p_mInj.K10.r1.xml 1 1
+lfmtpBatch.sh Inps/p_mInj.K15.r2.xml 2 2
+lfmtpBatch.sh Inps/p_mInj.K20.r3.xml 3 3
+lfmtpBatch.sh Inps/p_mInj.K25.r4.xml 4 4
+lfmtpBatch.sh Inps/p_mInj.K35.r5.xml 5 5
+lfmtpBatch.sh Inps/p_mInj.K50.r6.xml 6 6
+lfmtpBatch.sh Inps/p_mInj.K70.r7.xml 7 7
+lfmtpBatch.sh Inps/p_mInj.K100.r8.xml 8 8
+
