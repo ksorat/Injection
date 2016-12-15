0a3aa52e040b3deab0de2cbd86c38116fc39df32
ksorat <kareem.sorathia@gmail.com>
2016-12-15 16:47:41 -0500

    Fast push

--- hists/chkHist.py
+++ hists/chkHist.py
@@ -19,8 +19,8 @@ N = len(Stubs)
 Ks = []
 figName = "chK.png"
 figQ = 300
-K0 = 1; K1 = 200
-Nb = 50
+K0 = 1; K1 = 250
+Nb = 120
 
 for n in range(N):
 	h5p = Root + Stubs[n] + Tail
@@ -29,7 +29,7 @@ for n in range(N):
 	Ks.append(K)
 
 bins = np.linspace(K0,K1,Nb)
-plt.hist(Ks,bins,normed=False,log=True,stacked=True,fill=False)
+plt.hist(Ks,bins,normed=False,log=True,stacked=True,histtype='step',)
 plt.legend(Labs)
 plt.xlabel("Final Energy [keV]")
 plt.ylabel("Count")
