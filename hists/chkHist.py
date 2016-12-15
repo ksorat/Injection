e0fe5f7e0bbe0d8054f6a50ed9d6ada5834e5a82
ksorat <kareem.sorathia@gmail.com>
2016-12-15 16:08:58 -0500

    Fast push

---
+++ hists/chkHist.py
@@ -0,0 +1,37 @@
+#History of energies for charge differences
+
+import os
+import numpy as np
+import lfmViz as lfmv
+import lfmPostproc as lfmpp
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+
+Root = os.path.expanduser('~') + "/Work/Injection/Data/sInj/"
+Tail = "_sInj.K10.0001.h5part"
+Stubs = ["p","Hep","Hepp"]
+Labs = ["H+","He+","He++"]
+titS = "Single Injection (10 keV)"
+
+N = len(Stubs)
+
+Ks = []
+figName = "chK.png"
+figQ = 300
+K0 = 1; K1 = 150
+Nb = 100
+
+for n in range(N):
+	h5p = Root + Stubs[n] + Tail
+	pIds,K = lfmpp.getH5pFin(h5p,"kev")
+
+	Ks.append(K)
+
+bins = np.linspace(K0,K1,Nb)
+plt.hist(Ks,bins,normed=True,log=True)
+plt.legend(Labs)
+plt.xlabel("Final Energy [keV]")
+plt.ylabel("Population Density")
+plt.savefig(figName,dpi=figQ)
+plt.close('all')
