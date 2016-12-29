d6c545b7a9e98cdb7355caf7eba8d76c39035fa1
ksorat <kareem.sorathia@gmail.com>
2016-12-29 14:29:34 -0500

    Fast push

--- csInj/Kt.py
+++ csInj/Kt.py
@@ -24,8 +24,8 @@ for n in range(Ns):
 	Kb = K.mean(axis=1) #Average(t)
 	dK = K.std(axis=1) #Std
 	Kc = np.percentile(K,pC,axis=1)
-	#plt.errorbar(t,Kb,dK,label=Labs[n],errorevery=20)
-	plt.plot(t,Kc,label=Labs[n])
+	plt.errorbar(t,Kb,dK,label=Labs[n],errorevery=10)
+	#plt.plot(t,Kc,label=Labs[n])
 	print("Min = %f"%(K.min()))
 plt.legend()
 plt.savefig(figName,dpi=figQ)
